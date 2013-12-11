dc0286b2ff5841a29d892efd183f6cee2f2e5758
Sondre Eikanger Kvalø <zapodot@gmail.com>
2013-12-11 21:55:12 +0100

    Adding some real test conditions

--- src/test/java/org/constretto/camel/ConstrettoComponentInJavaContextTest.java
+++ src/test/java/org/constretto/camel/ConstrettoComponentInJavaContextTest.java
@@ -1,10 +1,15 @@
 package org.constretto.camel;
 
 import org.apache.camel.CamelContext;
+import org.apache.camel.EndpointInject;
+import org.apache.camel.Produce;
+import org.apache.camel.ProducerTemplate;
 import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.component.mock.MockEndpoint;
 import org.apache.camel.spring.javaconfig.SingleRouteCamelConfiguration;
 import org.apache.camel.test.spring.CamelSpringDelegatingTestContextLoader;
 import org.apache.camel.test.spring.CamelSpringJUnit4ClassRunner;
+import org.apache.camel.test.spring.MockEndpoints;
 import org.constretto.ConstrettoBuilder;
 import org.constretto.ConstrettoConfiguration;
 import org.constretto.model.Resource;
@@ -20,7 +25,10 @@ import org.springframework.test.context.ContextConfiguration;
 @RunWith(CamelSpringJUnit4ClassRunner.class)
 @ContextConfiguration(classes = ConstrettoComponentInJavaContextTest.Context.class,
         loader = CamelSpringDelegatingTestContextLoader.class)
+@MockEndpoints
 public class ConstrettoComponentInJavaContextTest {
+
+    public static final String TEST_BODY = "test";
 
     @Configuration
     public static class Context extends SingleRouteCamelConfiguration {
@@ -55,8 +63,17 @@ public class ConstrettoComponentInJavaContextTest {
         }
     }
 
+    @Produce(uri = "direct:input")
+    private ProducerTemplate producerTemplate;
+
+    @EndpointInject(uri = "mock:log:loggerName")
+    private MockEndpoint endpoint;
+
     @Test
     public void testRoute() throws Exception {
+        endpoint.expectedBodiesReceived(TEST_BODY);
+        producerTemplate.sendBody(TEST_BODY);
+        endpoint.assertIsSatisfied();
 
 
     }
